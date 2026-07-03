51b9bf3ef5a0b1dc10ce6846358e6eb3a05649cc
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-03 12:59:01 +0530

    Initial commit

---
+++ name.py
@@ -0,0 +1,20 @@
+#Question 5: 
+#Python 
+# Take your full name as input from user. 
+# Print the following: 
+# 1. Full name in uppercase 
+# 2. Full name in lowercase 
+# 3. Length of your name 
+# 4. First 3 characters 
+# 5. Last 3 characters
+name = 'Rampally Greesha Vaishnavi'
+new_name = name.upper()
+print(new_name)
+new_name = name.lower()
+print(new_name)
+len_name = len(name)
+print(len(name))
+
+print(name[0 :3])
+print(name[:-3])
+
