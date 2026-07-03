36240f853c5b9d45252661de4a9eb42aad67bbf4
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-03 13:42:03 +0530

    Initial commit

---
+++ slicing.py
@@ -0,0 +1,14 @@
+#Question 6: 
+#Python 
+# Take any sentence from user. 
+# Print: 
+# - First 10 characters 
+# - Last 10 characters   
+# - Characters from index 5 to 15 
+# - Every 2nd character of the sentence 
+
+message = ("python is a programming language")
+print(message[0:10])
+print(message[-10:])
+print(message[5:10])
+print(message[::2])
