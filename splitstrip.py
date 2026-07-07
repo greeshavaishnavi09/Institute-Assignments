291146bd78baf9d2a954b94b0adc6990cbdca908
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-07 23:04:25 +0530

    Initial commit

---
+++ splitstrip.py
@@ -0,0 +1,5 @@
+# Take a comma-separated string like "apple,banana,mango" and split it using split().
+
+text = "apple,banana,mango" 
+new_text = text.split(",")
+print(new_text)
---
+++ startend.py
@@ -0,0 +1,8 @@
+# Take a sentence and check whether it starts with "Hello" using startswith().
+# Take a string and check if it ends with ".com" using endswith().
+
+name = "Hello greesha@.com"
+new_name = name.startswith("Hello")
+print(new_name)
+new_name1 = name.endswith(".com")
+print(new_name1)
