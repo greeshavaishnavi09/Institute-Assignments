dd85eac9b8d8b480d88cfed13934bf2c4f9049ff
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-07 23:53:37 +0530

    Initial commit

---
+++ firstlastname.py
@@ -0,0 +1,5 @@
+# Take a full name (e.g. "Rahul Kumar Sharma") and print only first and last name using split().
+
+name = "Rampally Greesha Vaishnavi"
+new_name = name.split()
+print(f"{new_name[0]} {new_name[-1]}")
