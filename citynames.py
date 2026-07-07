5c74ef25b70ad8b3577e665842d69c27b720a817
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-08 00:20:27 +0530

    Initial commit

---
+++ citynames.py
@@ -0,0 +1,3 @@
+# Create a list of 5 city names from user input. Join them with new line "\n" and print.
+list = ["Hyderabad","Medchal","Secunderabad","Hightechcity","Madhapur"]
+print("\n".join(list))
