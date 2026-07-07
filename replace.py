20578279115df6391540c7680dc0d6a22c9e35f5
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-07 22:28:25 +0530

    Initial commit

---
+++ replace.py
@@ -0,0 +1,6 @@
+# Take a string from user and replace all spaces with underscore _ using replace() method.
+
+name = "my name is greesha"
+new_name = name.replace("greesha","vaishnavi")
+print(new_name)
+
