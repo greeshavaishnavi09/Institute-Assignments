bc47c797cbb0d08cfd8b86f62221f39eabdaeff9
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-07 23:58:15 +0530

    Initial commit

---
+++ totalwords.py
@@ -0,0 +1,6 @@
+#Take a sentence from user, split it into words, and print the total number of words.
+
+words = " python is a programming lang"
+new_words = words.split()
+total= len(new_words)
+print(total)
