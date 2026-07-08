c72a98c49f4b841e47af994701ea740af8beb4f2
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-08 21:14:33 +0530

    Initial commit

---
+++ Task3/append.py
@@ -0,0 +1,6 @@
+#Create an empty list and append 3 numbers into it using append().
+lists = []
+lists.append(20)
+lists.append(15)
+lists.append(16)
+print(lists)
---
+++ Task3/extend.py
@@ -0,0 +1,5 @@
+# Given lst = [1, 2, 3], use extend() to add [4, 5, 6] to it.
+
+lst = [1, 2, 3]
+lst.extend([4, 5, 6])
+print(lst)
