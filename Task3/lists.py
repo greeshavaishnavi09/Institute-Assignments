4bedf23d0f18d98f4312ae1c27d25dd26c014674
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-08 23:40:31 +0530

    Initial commit

---
+++ Task3/lists.py
@@ -0,0 +1,6 @@
+# Take a sentence from user (e.g. "I love Python programming") and convert it into a list of words using split().
+
+
+lists = "I love Python programming"
+words_lists = lists.split()
+print(words_lists)
