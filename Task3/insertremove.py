fa5a4e578f0ee3555485bcc1ac4b8aa1125d0a5a
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-08 23:54:04 +0530

    Initial commit

---
+++ Task3/insertremove.py
@@ -0,0 +1,7 @@
+# Create a list. Insert "Python" at index 0, then remove the last element using pop(), and print the length.
+
+lists = ["is" ,"a", "prog" ,"lan"]
+lists.insert(0,"Python")
+lists.pop(4)
+length_lists = len(lists)
+print(lists, length_lists)
