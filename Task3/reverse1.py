5862298f597b035a43721640fb8c5385d50a1c09
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-09 00:18:13 +0530

    Initial commit

---
+++ Task3/reverse1.py
@@ -0,0 +1,5 @@
+#Take 5 city names as input from user and store them in a list. Then print them in reverse order.
+
+city = ["Hyderabad","Pune","Mumbai","Chennai","Delhi"]
+city.reverse()
+print(city)
