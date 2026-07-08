f964ae4047976b9e4e4d61a7cca3cfcb0df70355
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-08 23:44:41 +0530

    Initial commit

---
+++ Task3/appendsort.py
@@ -0,0 +1,8 @@
+# Create a list of 5 numbers. Append 3 more numbers, then sort it in descending order.
+
+lists = [56,23,89,6,28]
+lists.append(45)
+lists.append(41)
+lists.append(52)
+lists.sort(reverse=True)
+print(lists)
