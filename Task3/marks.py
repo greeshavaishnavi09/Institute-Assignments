d187c652e79f230395e42ecfe6b38e19f73d8472
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-09 07:49:02 +0530

    Initial commit

---
+++ Task3/marks.py
@@ -0,0 +1,10 @@
+#Create a list of student marks. Sort it, find the highest and lowest mark using list methods, and print them.
+
+lists = [60,35,42,96,70]
+lists.sort()
+lowest = lists[0]
+highest = lists[-1]
+
+print(lowest)
+print(highest)
+print(lists)
