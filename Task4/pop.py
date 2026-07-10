57c02fdf0a9d500497e8cb4162b3cf71fefa5324
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-10 11:12:33 +0530

    Initial commit

---
+++ Task4/pop.py
@@ -0,0 +1,5 @@
+# Remove the key "age" from dictionary student = {"name": "Rahul", "age": 22, "city": "Delhi"} using pop().
+
+student = {"name": "Rahul","age": 22, "city": "Delhi"}
+student.pop("age")
+print(student)
---
+++ Task4/popitem.py
@@ -0,0 +1,7 @@
+# Use popitem() on dictionary d = {"x": 10, "y": 20, "z": 30} and print the removed item.
+
+d = {"x": 10, "y": 20, "z": 30}
+d.popitem()
+print(d)
+
+
