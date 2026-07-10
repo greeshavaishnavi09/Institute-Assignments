ed52104bf8821ad1a940e4969b13a6780fdbaf04
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-10 07:38:38 +0530

    Initial commit

--- Task4/key.py
+++ Task4/key.py
@@ -1,5 +1,4 @@
 #Given d = {"a": 1, "b": 2, "c": 3}, print all keys using keys() method.
 
 d={"a": 1, "b": 2, "c": 3}
-d.keys()
-print(d)
+print(d.keys())
---
+++ Task4/value.py
@@ -0,0 +1,4 @@
+# Given d = {"a": 1, "b": 2}, print all values using values() method.
+
+d = {"a": 1, "b": 2}
+print(d.values())
