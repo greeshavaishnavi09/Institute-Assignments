9a81719a4777b535490b2f727408a356379815aa
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-10 14:40:10 +0530

    Initial commit

---
+++ Task4/fromkey.py
@@ -0,0 +1,5 @@
+#Use fromkeys() to create a dictionary with keys ["red", "green", "blue"] and default value 0.
+
+
+keys =  ["red", "green", "blue"]
+print(dict.fromkeys(keys,0))
---
+++ Task4/tup.py
@@ -0,0 +1,5 @@
+#Create a tuple with mixed data types and try to change one element (show error).
+
+tup = (54,9.7,"Greesha",76,6.8,"Vaish")
+tup.index(1)
+print(tup)
