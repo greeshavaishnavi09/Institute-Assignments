587f2f522fc709c6d1f6abdb3145c1988398812a
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-10 08:39:08 +0530

    Initial commit

---
+++ Task4/empty.py
@@ -0,0 +1,9 @@
+# Create an empty dictionary and add 3 key-value pairs using assignment.
+
+dicts= {}
+dicts.update({"name":"Greesha"})
+dicts.update({"Age":"20"})
+dicts.update({"Gender":"Female"})
+print(dicts)
+
+
