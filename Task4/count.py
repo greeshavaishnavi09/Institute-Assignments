22a4171f3c9a6cef54faddafb60085e309719c96
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-10 08:23:03 +0530

    Initial commit

---
+++ Task4/count.py
@@ -0,0 +1,3 @@
+# Create a tuple tup = (10, 20, 30, 40, 50) and use count() to count how many times 20 appears.
+tup = (10, 20, 30, 40, 50)
+print(tup.count(20))
---
+++ Task4/index.py
@@ -0,0 +1,3 @@
+# Use index() method on tuple (10, 20, 30, 40) to find the position of 30.
+tup = (10, 20, 30, 40)
+print(tup.index(30))
