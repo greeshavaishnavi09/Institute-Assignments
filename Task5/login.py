a4f079cad9fba0b689ac594a05ed81fa6174544d
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-10 23:46:53 +0530

    Initial commit

---
+++ Task5/login.py
@@ -0,0 +1,12 @@
+#Take username, password, and age as input.
+# If username == "admin" and password == "python123" and age >= 18 → Print "Login Successful"
+# Else if age < 18 → Print "You are too young to login" Else → Print "Invalid Credentials"
+
+username = "Greesha"
+password = "python123"
+age = 25
+if username == "Greesha" and password == "python123" and age >= 18:
+    print("Login successfull")
+else:
+    print("Invalid credentials")
+
