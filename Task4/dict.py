df325c7da1761c7e3a3e236cb8a16f2918f258dd
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-10 07:15:19 +0530

    Initial commit

---
+++ Task4/dict.py
@@ -0,0 +1,4 @@
+#Create a dictionary student with keys: "name", "age", "city" and print it.
+
+dicts = {"name":"Greesha","Age":23,"city":"Hyd"}
+print(dicts)
---
+++ Task4/key.py
@@ -0,0 +1,5 @@
+#Given d = {"a": 1, "b": 2, "c": 3}, print all keys using keys() method.
+
+d={"a": 1, "b": 2, "c": 3}
+d.keys()
+print(d)
