4b5691ee7d2fe497bbb1abe34b14611808f52f08
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-11 19:03:23 +0530

    Initial commit

---
+++ Task6/salary.py
@@ -0,0 +1,10 @@
+# Take salary as input. If salary > 50000 print "High", elif salary > 30000 print "Medium", else print "Low".
+
+sal = int(input("Enter the salary:"))
+
+if sal >50000:
+    print("High")
+elif sal >30000:
+    print("Medium")
+else:
+    print("Low")    
