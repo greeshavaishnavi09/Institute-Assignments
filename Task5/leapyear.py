39f17abd512e519931390e40ac9abfe74a9b4ee2
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-11 16:08:25 +0530

    Initial commit

--- Task5/leapyear.py
+++ Task5/leapyear.py
@@ -1,5 +1,8 @@
 #Take a year as input. Check if it is a leap year or not. (A year is leap if divisible by 4, but not by 100, unless also divisible by 400)
 
-year= int(input("Enter a year"))
-month_days=[0,31,28,31,30,31,30,31,31,30,31,30,31]
-for n in year % 4 == 0 and (year% 100 ==0 oryear% 400 ==0 ):
+year= 2020
+
+if (year % 4 == 0 and year% 100!=0) or (year% 400 ==0 ):
+    print("Leap Year")
+else:
+    print("Not a Leap Year")
---
+++ Task5/length.py
@@ -0,0 +1,6 @@
+#Create a set from the list [1, 2, 2, 3, 4, 4, 5] and print its length.
+
+list = [1, 2, 2, 3, 4, 4, 5]
+list = set(list)
+length = len(set)
+print(set)
