3e66136022572e5a932bcd083d28b8741991acad
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-11 20:27:57 +0530

    Initial commit

---
+++ Task6/num.py
@@ -0,0 +1,12 @@
+#Take a number and print whether it is positive, negative, or zero.
+ 
+num = int(input("Enter a num:"))
+
+if num >0:
+    print("poitive")
+elif num< 0 :
+    print("negative")
+elif num==0:
+    print("zero")      
+else:
+    print("none")     
