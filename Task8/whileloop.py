cc9db9b883119de9e89c421d2998baf3016dfc44
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-14 22:16:53 +0530

    Initial commit

--- Task8/whileloop.py
+++ Task8/whileloop.py
@@ -1,2 +1,14 @@
 #Take a number n from the user. Using while loop, print all prime numbers between 2 and n.
 
+num = 2
+
+prime_num =[]
+while num <=10:
+    if num % 2==0:
+        prime_num.append("prime numbers")
+
+    else:
+        prime_num.append("Not a prime numbers")
+
+    num+=1
+print(prime_num)        
