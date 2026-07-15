f4dbf808c2d772fce1dbeb9d853c377716a256ac
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-15 22:59:37 +0530

    Initial commit

---
+++ Task9/descending.py
@@ -0,0 +1,6 @@
+# Print numbers from 50 to 30 in descending order using for loop.
+
+num = 1
+
+for i in range(50,29,-1):
+    print(i)
---
+++ Task9/num.py
@@ -0,0 +1,8 @@
+# Print numbers from 1 to 15 using while loop.
+
+n = 1
+while n >= 15:
+
+    n+=1
+print(n)
+
