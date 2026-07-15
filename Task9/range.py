aa5869e649d8d0b1a1bdfa257bbec1d06a73f791
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-15 16:57:22 +0530

    Initial commit

---
+++ Task9/range.py
@@ -0,0 +1,5 @@
+# Print numbers from 1 to 10 using for loop.
+
+num = 1
+for num in range(1,11):
+    print(num)
---
+++ Task9/sum.py
@@ -0,0 +1,9 @@
+#Take a number n from user and print sum of first n natural numbers using for loop.
+
+num =5
+sum=0
+
+for num in range(1,num+1):
+    if num>=1:
+        sum(num)
+    print(num)    
