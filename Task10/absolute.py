9988cfd5161999bb1443433ec3e57db5871c4891
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-16 14:41:46 +0530

    Initial commit

---
+++ Task10/absolute.py
@@ -0,0 +1,4 @@
+# What is the output of abs(-25) and abs(7.8)?
+
+print(abs(-25))
+print(abs(7.8))
---
+++ Task10/round.py
@@ -0,0 +1,5 @@
+# What does round(3.678, 2) return? What about round(3.5) and round(2.5)?
+
+print(round(3.678,2))
+print(round(3.5))
+print(round(2.5))
