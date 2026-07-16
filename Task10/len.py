2906c137682a691c1e076c289577f3e312ca8b0b
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-16 14:23:12 +0530

    Initial commit

---
+++ Task10/len.py
@@ -0,0 +1,8 @@
+# What is the output of len("Python"), len([1,2,3,4]), and len((10,20))?
+
+str = "python"
+list1 = [1,2,3,4]
+list2 = (10,20)
+print(len(str))
+print(len(list1))
+print(len(list2))
---
+++ Task10/type.py
@@ -0,0 +1,6 @@
+# What will type(10) and type(3.14) return? What is the difference?
+
+a = 45
+b = 4.05
+print(type(a))
+print(type(b))
