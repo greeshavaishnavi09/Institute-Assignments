4f21b1bad4fad0530aa744a826fbf899a1d0776d
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-16 14:38:15 +0530

    Initial commit

---
+++ Task10/instancetype.py
@@ -0,0 +1,4 @@
+# What is the difference between type() and isinstance()? Give an example.
+
+a = 10
+type(a)
---
+++ Task10/predict.py
@@ -0,0 +1,2 @@
+# Predict the output: print(len([[1,2],[3,4]]))
+print(len([[1,2],[3,4]]))
