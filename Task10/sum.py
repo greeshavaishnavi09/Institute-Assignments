cf2446304a635c0dbdd8188eec294ac41615d32e
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-16 14:59:46 +0530

    Initial commit

---
+++ Task10/sum.py
@@ -0,0 +1,8 @@
+# 10.What will sum([1, 2, 3, 4, 5]) return? Can sum() work on a list of strings?
+
+lists = [1, 2, 3, 4, 5]
+print(sum(lists))
+
+str = ["python"]
+print(sum(str))
+# No the sum() function cannot work on a list of strings
