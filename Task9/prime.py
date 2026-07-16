7982ab41a0c3ef03ec2c8c4949afffe7e67db06f
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-16 14:05:29 +0530

    Initial commit

---
+++ Task9/prime.py
@@ -0,0 +1,13 @@
+# 12. Check if a number is prime using while loop.
+
+num =0
+
+while num in range(1,10):
+    if num % 2==0:
+        print("prime num")
+
+        num+=1
+else:
+    print("Not a prime number") 
+
+
---
+++ Task9/reversenum.py
@@ -0,0 +1,15 @@
+# 11. Reverse a number using while loop.
+
+num =1234
+rev = 0
+
+while num > 0:
+    digit = num % 10
+    rev = rev*10 +digit
+    num = num//10
+print(rev)    
+
+
+
+
+
