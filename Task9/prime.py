373b590d6aea2512dff19b926a49ab2e9e543eb7
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-16 14:47:09 +0530

    Initial commit

--- Task9/prime.py
+++ Task9/prime.py
@@ -1,12 +1,12 @@
 # 12. Check if a number is prime using while loop.
 
-num =0
+num =4
 
 while num in range(1,10):
-    if num % 2==0:
+    if num % num ==0:
         print("prime num")
 
-        num+=1
+    num+=1
 else:
     print("Not a prime number") 
 
