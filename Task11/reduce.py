0bddb6b020b2b4adb482cf150ec627a179880caf
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-20 10:37:38 +0530

    project structure

--- Task11/reduce.py
+++ Task11/reduce.py
@@ -5,3 +5,9 @@ lists = [10,20,30,40]
 
 result = reduce(lambda x, y: x + y, [10,20,30,40])
 print(result)
+
+#Use reduce() with lambda to find the product of all numbers.
+lists1 = [2,3,4,5]
+
+result1 = reduce(lambda x,y : x*y,[2,3,4,5])
+print(result1)
