d66d570d423fe9b8723c90c35415cb2f4a01badb
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-21 21:54:11 +0530

    Initial commit

---
+++ Task13/bank.py
@@ -0,0 +1,6 @@
+class BankAccount:
+    def __init(self,balance):
+        self.__balance= balance
+
+bank = BankAccount(500)
+print(bank._BankAccount) 
---
+++ Task13/stu_private.py
@@ -0,0 +1,6 @@
+class Student:
+    def __init__(self,marks):
+        self.__marks= marks
+
+stu = Student(86)
+print(stu._Student__marks)        
