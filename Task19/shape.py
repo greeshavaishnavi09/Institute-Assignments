58d14ae84efd1db69e6002795aef62aa937dacf2
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-07-28 14:57:33 +0530

    Task 19

--- Task19/shape.py
+++ Task19/shape.py
@@ -1,55 +1,48 @@
+import math
+
 class Circle:
-    def pay(Self,r):
-        print(f"required area to construct a circle:{r}")
-       
+    def __init__(self,r):
+        self.r = r
+
+    def draw(self):
+        print("Drawing a circle")
+
+    def area(self):
+        print("area of circle", math.pi* self.r **2 )
+
 class Rectangle:
-    def pay(Self,width,height):
-        print(f"required area to construct a circle:{width}")
-        print(f"required area to construct a circle:{height}")  
+    def __init__(self,width,height):
+        self.width = width
+        self.height = height
+
+    def draw(self):
+        print("Drawing a Rectangle")
+
+    def area(self):
+        print("area of Rectangle", self.width* self.height ) 
+
 
 class Triangle:
-    def pay(Self,base,height):
-        print(f"required area to construct a circle:{base}")
-        print(f"required area to construct a circle:{height}")  
+    def __init__(self,base,height):
+        self.base = base
+        self.height = height
 
-# make function
-def draw(draw,r):
-    draw.pay(r) 
-def area(area,r):
-    area.pay(r)   
+    def draw(self):
+        print("Drawing a Triangle")
 
-def draw(draw,width):
-    draw.pay(width) 
-def area(area,width):
-    area.pay(width)
-
-def draw(draw,height):
-    draw.pay(height) 
-def area(area,height):
-    area.pay(height)
-
-def draw(draw,base):
-    draw.pay(base) 
-def area(area,base):
-    area.pay(base)
+    def area(self):
+        print("area of Triangle", 0.5 * self.base * self.height ) 
 
 
+def display(shape):
+    shape.draw()
+    shape.area()   
 
 
+c = Circle(3)
+r = Rectangle(4,6)
+t = Triangle(4,5)
 
-
-
-
-# create objects
-
-c= Creditcard()
-d= Debitcard()
-u = Upi()
-
-make_payement(c,500)
-make_payement(d,500)
-make_payement(u,500)
-
-print(c.pay(100))
-print(d.pay(500))
-print(u.pay(300))
+display(c)
+display(r)
+display(t)
