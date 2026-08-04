bdaf2cb8d502ddfd6c6132baa6cb5122eda6983a
GreeshaVaishnavi <greeshavaishnavi09@gmail.com>
2026-08-04 20:53:12 +0530

    Task 24

---
+++ Task24/numiter.py
@@ -0,0 +1,16 @@
+# Number Iterator
+
+numbers = range(1, 11)
+
+it = iter(numbers)
+
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
